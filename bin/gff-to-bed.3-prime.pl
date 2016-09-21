8abe17586ebd398bab4e90ccefbd46871eb2d774
Mike Covington <mfcovington@gmail.com>
2016-09-21 10:31:10 -0700

    Get options from CLI

--- bin/gff-to-bed.3-prime.pl
+++ bin/gff-to-bed.3-prime.pl
@@ -8,21 +8,28 @@ use strict;
 use warnings;
 use autodie;
 use feature 'say';
+use Getopt::Long;
 use List::Util qw(min max);
 
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use gff;
 
-# TEMPORARY Defaults
-my $gff_file = glob "~/git.repos/extract-utr/Zea_mays.AGPv3.21.gff3";
-# my $gff_file = glob "~/git.repos/extract-utr/GRMZM2G176820.gff";
-my $bed_file = glob "~/git.repos/extract-utr/out2.bed";
+# Default Options
+my ( $gff_in_file, $bed_out_file );
 my $cds_length = 500;
 my $utr_length = 500;
-my $transcript_delimiter = "_";
+my $transcript_delimiter = "_T";
 
-my $coding_regions = extract_cds_from_gff($gff_file);
+my $options = GetOptions(
+    "gff_in_file=s"          => \$gff_in_file,
+    "bed_out_file=s"         => \$bed_out_file,
+    "cds_length=i"           => \$cds_length,
+    "utr_length=i"           => \$utr_length,
+    "transcript_delimiter=s" => \$transcript_delimiter,
+);
+
+my $coding_regions = extract_cds_from_gff($gff_in_file);
 convert_coding_regions_to_three_prime( $coding_regions, $cds_length,
     $utr_length );
 
@@ -48,9 +55,9 @@ for my $transcript_id ( keys %$coding_regions ) {
     $genes{$gene_id}{'strand'} = $$coding_regions{$transcript_id}{'strand'};
 }
 
-open my $bed_fh, ">", $bed_file;
+open my $bed_out_fh, ">", $bed_out_file;
 for my $gene_id ( sort keys %genes ) {
-    say $bed_fh join "\t",
+    say $bed_out_fh join "\t",
         $genes{$gene_id}{'chr'},
         $genes{$gene_id}{'left_pos'},
         $genes{$gene_id}{'right_pos'},
@@ -58,4 +65,4 @@ for my $gene_id ( sort keys %genes ) {
         "",
         $genes{$gene_id}{'strand'};
 }
-close $bed_fh;
+close $bed_out_fh;
